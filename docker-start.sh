19a34f22f800ba69540b6f499d6c7c55412a021d
Luca Pisani <luca.pisani@abstract.it>
2016-07-07 12:54:05 +0200

    mv config apps in config

--- docker-start.sh
+++ docker-start.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-cd /code/config
+mkdir templates && cd templates
 if [ -d ".git" ]; then
    git pull
 else
    git clone $github_repo .
 fi 
+ln -sf templates/config /code/config
 chmod 777 /code/config
 chmod -R 775 /code/config/*
