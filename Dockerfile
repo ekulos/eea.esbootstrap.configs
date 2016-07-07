630b5fd34febd7a118bbbd71fa6baea7d2d271c8
Luca Pisani <luca.pisani@abstract.it>
2016-07-07 13:10:36 +0200

    revert

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,4 @@ RUN apk update && apk upgrade && \
     apk add --no-cache bash git openssh
 
 ADD ./docker-start.sh /
-WORKDIR /code/config
 CMD /docker-start.sh
--- docker-start.sh
+++ docker-start.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
-mkdir templates && cd templates
+cd /code/config
 if [ -d ".git" ]; then
    git pull
 else
    git clone $github_repo .
 fi 
-ln -sf templates/config /code/config
 chmod 777 /code/config
 chmod -R 775 /code/config/*
